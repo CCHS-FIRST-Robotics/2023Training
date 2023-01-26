6b90e3c01bfe86c6d7c89db9b001f397edecf7ff
hmwang2305@gmail.com <hmwang2305@gmail.com>
2023-01-26 16:38:38 -0500

    main branch is random code

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public  class Constants {
+    public static final int XBOX_PORT = 0; 
+
+    public static final int FR_TALON_PORT = 12;
+	public static final int FL_TALON_PORT = 13;
+	public static final int RR_TALON_PORT = 1;
+	public static final int RL_TALON_PORT = 2;
+}
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,82 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends TimedRobot {
+  public final PWMTalonSRX flTalonMotor = new PWMTalonSRX(Constants.FL_TALON_PORT);
+  public final PWMTalonSRX rlTalonMotor = new PWMTalonSRX(Constants.RL_TALON_PORT);
+  public final PWMTalonSRX frTalonMotor = new PWMTalonSRX(Constants.FR_TALON_PORT);
+  public final PWMTalonSRX rrTalonMotor = new PWMTalonSRX(Constants.RR_TALON_PORT);
+
+  private final MecanumDrive mecaDrive = new MecanumDrive(flTalonMotor, rlTalonMotor, frTalonMotor, rrTalonMotor);
+  private final XboxController m_controller = new XboxController(Constants.XBOX_PORT);
+  private final Timer m_timer = new Timer();
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    flTalonMotor.setInverted(true);
+    rlTalonMotor.setInverted(true);
+  }
+
+  /** This function is run once each time the robot enters autonomous mode. */
+  @Override
+  public void autonomousInit() {
+    m_timer.reset();
+    m_timer.start();
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    // Drive for 2 seconds
+    if (m_timer.get() < 2.0) {
+      // Drive forwards half speed, make sure to turn input squaring off
+     // mecaDrive.driveCommand(0.5, 0.0, false);
+    } else {
+      mecaDrive.stopMotor(); // stop robot
+    }
+  }
+
+  /** This function is called once each time the robot enters teleoperated mode. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during teleoperated mode. */
+  @Override
+  public void teleopPeriodic() {
+    mecaDrive.driveCartesian(m_controller.getLeftX(), m_controller.getLeftY(), m_controller.getRightX());
+  }
+
+  /** This function is called once each time the robot enters test mode. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+}
