e518583f76a24856dcdda9f975560af3571be85d
Alexandre Hus-Shao <alexjun2008@gmail.com>
2023-03-21 15:10:15 -0400

    r

--- src/main/java/frc/robot/subsystems/Lidar.java
+++ src/main/java/frc/robot/subsystems/Lidar.java
@@ -1,3 +1,7 @@
-public class Lidar {
+package frc.robot;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Lidar extends TimedRobot{
     
 }
