9e076f8a07c8fc2349253351b33128c2dead95cf
Alexandre Hus-Shao <alexjun2008@gmail.com>
2023-03-21 15:15:49 -0400

    hi

--- src/main/java/frc/robot/subsystems/Lidar.java
+++ src/main/java/frc/robot/subsystems/Lidar.java
@@ -1,7 +1,31 @@
-package frc.robot;
+package frc.robot.subsystems;
+
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Lidar extends TimedRobot{
-    
+    private Counter m_LIDAR;
+    @Override
+    public void robotInit() {
+        m_LIDAR = new Counger(0);
+        m_LIDAR.setMaxPeriod(1.00);
+        m_LIDAR.setSemiPeriodMode(true);
+        m_Lidar.reset();
+    }
+    final double off = 10;
+
+    @Override
+    public void robotPeriodic() {
+        double dist;
+        if(m_LIDAR.get() < 1){
+            dist=0;
+        }
+        else {
+            dist=(m_LIDAR.getPeriod()*1000000.0/10.0)-off;
+
+            if(dist<20){
+                System.out.println("piece in the hole");
+            }
+        }
+    }
 }
